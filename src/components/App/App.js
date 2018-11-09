0e2ae6245e37cc98c8df62cbc6cfe223c08f3332
Victor Ferraz <victorfsf.dev@gmail.com>
2018-11-09 10:43:09 -0300

    Update folder structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import styles from './App.css';
+import logo from 'assets/logo.svg';
+import styles from './styles.css';
 
 class App extends Component {
   render() {
